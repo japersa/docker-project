873fd8af4941f05a33ea5cc5f81a1a01411ab992
Alvaro Javier Perez Salcedo <japersa92@gmail.com>
2024-12-01 22:23:58 -0500

    Agregar pruebas para la conexión a la base de datos y la respuesta de la API

--- __test__/index.test.js
+++ __test__/index.test.js
@@ -1,5 +1,45 @@
-describe('index', () => {
-    it('should return 1', () => {
-        expect(1).toBe(1);
+const request = require('supertest');
+const mysql = require('mysql2');
+const app = require('../index');
+const e = require('express');
+
+jest.mock('mysql2', () => {
+    const mockConnection = {
+        connect: jest.fn((callback) => callback(null)),
+        query: jest.fn(),
+        end: jest.fn()
+    };
+    return {
+        createConnection: jest.fn(() => mockConnection)
+    };
+});
+
+describe('App test', () => {
+    let connection;
+
+    beforeAll(() => {
+        connection = mysql.createConnection();
     });
+
+    afterAll(() => {
+        connection.end();
+    });
+
+    test('should connect to database', () => {
+        expect(connection.connect).toHaveBeenCalled();
+    });
+
+    test('should return a list of users on GET /', async () => {
+        const users = [{ id: 1, name: 'John' }, { id: 2, name: 'Jane' }];
+        connection.query.mockImplementation((query, callback) => {
+            if (query === 'SELECT * FROM users') callback(null, users);
+        });
+
+        const response = await request(app).get('/');
+        expect(response.body).toEqual(users);
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual(users);
+        expect(connection.query).toHaveBeenCalledWith('SELECT * FROM users', expect.any(Function));
+    });
+
 });
